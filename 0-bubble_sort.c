0174c8fcba545bd9d48aa7758d2d032a9b558ad3
islamhasib953 <islamhasib953@gmail.com>
2024-01-28 00:29:10 +0200

    sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,17 +2,17 @@
 
 /**
  * swap - swap function
- * @x: The Position Of Previous Element
- * @y: The Position Of Current Element
+ * @xp: The Position Of Previous Element
+ * @yp: The Position Of Current Element
  *
  * Return: void
  */
 
-void swap(int *x, int *y)
+void swap(int *xp, int *yp)
 {
-	size_t tmp = *x;
-	*x = *y;
-	*y = tmp;
+	size_t tmp = *xp;
+	*xp = *yp;
+	*yp = tmp;
 }
 /**
  * bubble_sort - sorting array using bubble algorithm
@@ -24,15 +24,25 @@ void swap(int *x, int *y)
 
 void bubble_sort(int *array, size_t size)
 {
-    for (int i =0; i< size; i++)
-    {
-        for (int j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                swap(&array[j], &array[j + 1]);
-                print_array(array, size);
-            }
-        }
-    }
+	size_t i, j;
+	bool swapped;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swapped = true;
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+		if (swapped == false)
+			break;
+	}
 }
